87bbcb7dfcc82feddc16652f2f74e078521446b6
moore-brandon4 <moore_brandon4@columbusstate.edu>
2021-08-25 20:20:28 +0000

    added multiply and divide functions to calculate.py

--- calculator.py
+++ calculator.py
@@ -4,22 +4,32 @@ def add(x, y):
 def subtract(x, y):
     return x-y
 
-#def multiply
+def multiply(x, y):
+    return x*y
 
-#def divide
+def divide(x, y):
+    return x/y
 
 print("Select Operation,")
-print("1.Add")
-print("2.Subtract")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("2. Divide")
 
 while True:
     choice = input("Enter choice (1,2,3,4):")
-    if choice in ('1','2'):
+    if choice in ('1','2','3','4'):
         num1 = float(input("Enter a number: "))
         num2 = float(input("Enter a second number: "))
 
         if choice =='1':
             print(num1,"+",num2,"=", add(num1,num2))
+        elif choice =='2':
+            print(num1,"-",num2,"=", subtract(num1,num2))
+        elif choice =='3':
+            print(num1,"*",num2,"=", multiply(num1,num2))
+        elif choice =='4':
+            print(num1,"/",num2,"=", divide(num1,num2))
         break
     else:
         print("Invalid Input")
